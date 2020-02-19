5cb00f0866c82aacd6eeff2990ab81bc07a6689c
lcruz94 <61032783+lcruz94@users.noreply.github.com>
2020-02-19 16:57:20 -0600

    mongo db 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace mongodbtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MongoUrl url = new MongoUrl("mongodb://localhost");
+            MongoClient mc = new MongoClient("mongodb://localhost");
+            MongoServer mongo = mc.GetServer();
+            var bbdds = mongo.GetDatabaseNames();
+            foreach (string bbdd in bbdds)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
