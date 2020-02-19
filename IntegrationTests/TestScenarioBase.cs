df358bfb4dd259365980bc87923bd8daca791b90
Erick190005 <61066528+Erick190005@users.noreply.github.com>
2020-02-19 14:34:26 -0600

    Xunit

---
+++ IntegrationTests/TestScenarioBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace IntegrationTests
+{
+    public class TestScenarioBase
+    {
+        public TestServer CreateServer()
+{   
+  var webHostBuilder = WebHost.CreateDefaultBuilder();
+  webHostBuilder.UseStartup<IStartup>();
+  var testServer = new TestServer(webHostBuilder);
+
+  return testServer;
+}
+    }
+}
---
+++ IntegrationTests/TestScenarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class TestScenarios: TestScenarioBase
+    {
+        [Fact]
+        public async Task GetTest()
+        {
+            using (var server = CreateServer())
+            {
+                var response = await server.CreateClient()
+                .GetAsync($"api/values/5");
+
+                Assert.Equal("value", await response.Content.ReadAsStringAsync());
+            }
+        }
+    }
+}
