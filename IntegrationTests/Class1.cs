d7f9c95ba56c816579c188c42999b8d5a2484596
Erick190005 <61066528+Erick190005@users.noreply.github.com>
2020-02-19 17:00:37 -0600

    Add files via upload

---
+++ IntegrationTests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace IntegrationTests
+{
+    c class Program
+    {
+        static void Main(string[] args)
+        {
+            //MongoUrl url = new MongoUrl("mongodb://localhost");
+            MongoClient mc = new MongoClient("mongodb://localhost");
+            MongoServer mongo = mc.GetServer();
+            var bbdds = mongo.GetDatabaseNames();
+            foreach (string bbdd in bbdds)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
