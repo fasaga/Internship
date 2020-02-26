8ba0a5f28292551baddb3cff2f04f51c203ec3b2
Adrian Sanchez <fabian.sanchez@agilethought.com>
2020-02-25 18:02:45 -0600

    architecture implementation

--- Internship.API/Controllers/WeatherForecastController.cs
+++ Internship.API/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Internship.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
---
+++ Internship.API/Models/ApiError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internship.API.Models
+{
+    public class ApiError
+    {
+        public int Code { get; set; }
+        public string Description { get;  set; }
+        public string Message { get;  set; }
+
+        public ApiError(int Code, string description)
+        {
+            this.Code = Code;
+            Description = description;
+        }
+
+        public ApiError(int code, string description, string message)
+        {
+            Code = code;
+            Description = description;
+            Message = message;
+        }
+
+    }
+}
