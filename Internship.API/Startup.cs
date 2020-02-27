6d87b037933b7211e8671703904e8e90ecb6562c
Luis Cruz <luis.cruz@agilethought.com>
2020-02-27 16:09:18 -0600

    Add MongoDB

--- Internship.API/Startup.cs
+++ Internship.API/Startup.cs
@@ -46,6 +46,18 @@ namespace Internship.API
             {
                 endpoints.MapControllers();
             });
+
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<BookstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+            services.AddControllers();
         }
     }
 }
