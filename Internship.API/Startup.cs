d8058a0404fbb33504cdbe2d1fb562ba02677e37
Luis Cruz <luis.cruz@agilethought.com>
2020-02-28 16:12:40 -0600

    Final Changes MongoDB

--- Internship.API/Startup.cs
+++ Internship.API/Startup.cs
@@ -26,11 +26,11 @@ namespace Internship.API
         public void ConfigureServices(IServiceCollection services)
         {
             // requires using Microsoft.Extensions.Options
-            services.Configure<BookstoreDatabaseSettings>(
-                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+            services.Configure<SourceSCDatabaseSettings>(
+                Configuration.GetSection(nameof(SourceSCDatabaseSettings)));
 
-            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
-                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+            services.AddSingleton<ISourceSCDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<SourceSCDatabaseSettings>>().Value);
             services.AddControllers();
         }
 
