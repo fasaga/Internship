2a87467948206213e21618783ebe6828887c5e48
Erick Ortega <erick.ortega@agilethought.com>
2020-03-03 16:53:34 -0600

    US 7: Create User Model

--- Internship.API/Models/User.cs
+++ Internship.API/Models/User.cs
@@ -9,22 +9,40 @@ namespace Internship.API.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        [BsonElement("first_name")]
+        
 
         public int UserId { get; set; }
+
+        [BsonElement("first_name")]
         [Required]
         public string FistName { get; set; }
+        
+        [BsonElement("last_name")]
         [Required]
         public string LastName { get; set; }
+        
+        [BsonElement("email")]
         [Required]
         public string Email { get; set; }
+        
+        [BsonElement("phone")]
         public string Phone { get; set; }
+        
+        [BsonElement("start_date")]
         [Required]
         public DateTime StartDate { get; set; }
+        
+        [BsonElement("End_date")]
         public DateTime EndDate { get; set; }
+        
+        [BsonElement("active_technology")]
         public string ActiveTechnology { get; set; }
+        
+        [BsonElement("status")]
         [Required]
         public string Status { get; set; }
+        
+        [BsonElement("role")]
         public string Role { get; set; }
 
     }
