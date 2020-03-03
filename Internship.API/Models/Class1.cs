9e1d7ad509313ae4c5d8c9fcc32947ffb926d95b
Luis Cruz <luis.cruz@agilethought.com>
2020-03-03 14:20:25 -0600

    US 10: Create Intern Model

---
+++ Internship.API/Models/Class1.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace InternApiModel.Models
+{
+    public class MentorModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        [BsonElement("Intern")]
+
+        public int UserId { get; set; }
+        public int MentorId { get; set; }
+        public string TechnologiesSummary { get; set; }
+        public int InitialEnglishLevel { get; set; }
+        public int FinalEnglishLevel { get; set; }
+        public boolean Hired { get; set; }
+        public int HiredSprintId { get; set; }
+        public string Location { get; set; }
+        public string fourthSourceOrg { get; set; }
+        public string Client { get; set; }
+        public string Project { get; set; }
+        public string Team { get; set; }
+        public string Lead { get; set; }
+        public string ResourceManager { get; set; }
+        public string Comment { get; set; }
+}
+
+public class TechnologiesSummary
+{
+    public string starting { get; set; }
+    public string final { get; set; }
+
+}
+}
