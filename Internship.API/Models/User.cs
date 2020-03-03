18104ec295a1ab210be90b3b13e84a1df315d4b9
Erick Ortega <erick.ortega@agilethought.com>
2020-03-03 14:25:14 -0600

    Create User Model

---
+++ Internship.API/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Internship.API.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        [BsonElement("Users")]
+
+        public int UserId { get; set; }
+        [Required]
+        public string FistName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ActiveTechnology { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+        public string Role { get; set; }
+    }
+}
