699a56b29837a4273dec3669a35f1514664e497d
pedro.ruiz <pedro.ruiz@agilethought.com>
2020-03-03 23:41:23 -0600

    US 14: Implement GET /api/mentors

---
+++ Internship.API/Controllers/MentorsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Internship.API.Models;
+using Internship.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Internship.API.Controllers
+{
+    [ApiController]
+    [Route("api/mentors")]
+    public class MentorsController : ControllerBase
+    {
+        private readonly MentorsService _mentorService;
+
+        public MentorsController(MentorsService mentorService)
+        {
+            _mentorService = mentorService;
+        }
+        [HttpGet]
+        public ActionResult<List<Mentor>> Get() =>
+           _mentorService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetMentor")]
+        public ActionResult<Mentor> Get(int id)
+        {
+            var mentor = _mentorService.Get(id);
+
+            if (mentor == null)
+            {
+                return NotFound();
+            }
+
+            return mentor;
+        }
+
+
+
+    }
+}
