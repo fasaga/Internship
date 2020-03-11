7583f1fd73603ab28c6990b499667b021036ab7a
pedro.ruiz <pedro.ruiz@agilethought.com>
2020-03-11 15:09:23 -0600

    US 9: Implement unit testing for create interns endpoint

---
+++ Internship.API.Tests/InternCreateEndpointTests.cs
@@ -0,0 +1,70 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Internship.API.Controllers;
+using Internship.API.Models;
+using Internship.API.Services.Interfaces;
+using System;
+using Moq;
+using Internship.API.Repositories.Interfaces;
+using Internship.API.Services;
+
+namespace Internship.API.Tests
+{
+    public class InternCreateEndpointTests : ControllerBase
+    {
+
+        [Fact]
+        public void Controller_Add_ValidData_Return_OkResult()
+        {
+            //============= Test to controller ==========================
+            var service = new Mock<IUserService>();
+            service.Setup(s => s.Create(It.IsAny<User>())).Returns((User user) => { return user; });
+
+            //Arrange  
+            var controller = new UsersController(service.Object);
+            var user = new User()
+            {
+                FirstName = "Lucia",
+                LastName = "Gomez",
+                Email = "lucia@hotmail.com",
+                StartDate = DateTime.Parse("2020-03-01T00:00:00Z"),
+                Status = "active",
+                Role = "intern"
+            };
+
+            //Act  
+            var data = controller.Create(user);
+
+            //Assert  
+            Assert.IsType<ActionResult<User>>(data);
+            Assert.NotNull(data);
+            Assert.True(data.Value.Email.Equals("lucia@hotmail.com"));
+        }
+        [Fact]
+        public void Service_Add_ValidData_Return_OkResult()
+        {
+            //============= Test to service ===========================
+            var service = new Mock<IUserRepository>();
+            service.Setup(s => s.Create(It.IsAny<User>())).Returns((User user) => { return user; });
+
+            //Arrange  
+            var controller = new UserService(service.Object);
+            var user = new User()
+            {
+                FirstName = "Lucia",
+                LastName = "Gomez",
+                Email = "lucia@hotmail.com",
+                StartDate = DateTime.Parse("2020-03-01T00:00:00Z"),
+                Status = "active",
+                Role = "intern"
+            };
+
+            //Act  
+            var data = controller.Create(user);
+
+            //Assert  
+            Assert.NotNull(data);
+            Assert.True(data.Email.Equals("lucia@hotmail.com"));
+        }
+    }
+}
