b64a817403b3011df449700cbadf4d565153cae7
Erick Ortega <erick.ortega@agilethought.com>
2020-03-13 11:42:27 -0600

    US 12: Implement unit testing for create users endpoint

--- Internship.API.Tests/UsersEndpoint.cs
+++ Internship.API.Tests/UsersEndpoint.cs
@@ -2,38 +2,42 @@
 using System;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
-
+using Internship.API.Models;
+using Internship.API.Controllers;
+using Internship.API.Services.Interfaces;
 
 namespace Internship.API.Tests
 {
-    class UsersEndpoint
+    public class UsersEndpoint
     {
-    }
-    [Fact]
-    public void Controller_Add_ValidData_Return_OkResult()
-    {
-        //============= Test to controller ==========================
-        var service = new Mock<IUserService>();
-        service.Setup(s => s.Create(It.IsAny<User>())).Returns((User user) => { return user; });
-
-        //Arrange  
-        var controller = new UsersController(service.Object);
-        var user = new User()
+        [Fact]
+        public void Controller_Add_ValidData_ReturnOkResult()
         {
-            FirstName = "Lucia",
-            LastName = "Gomez",
-            Email = "lucia@hotmail.com",
-            StartDate = DateTime.Parse("2020-03-01T00:00:00Z"),
-            Status = "active",
-            Role = "intern"
-        };
+            //============= Test to controller ==========================
+            var service = new Mock<IUserService>();
+            service.Setup(s => s.Create(It.IsAny<User>())).Returns((User user) => { return user; });
 
-        //Act  
-        var data = controller.Create(user);
+            //Arrange  
+            var controller = new UsersController(service.Object);
+            var user = new User()
+            {
+                FirstName = "Lucia",
+                LastName = "Gomez",
+                Email = "lucia@hotmail.com",
+                StartDate = DateTime.Parse("2020-03-01T00:00:00Z"),
+                Status = "active",
+                Role = "intern"
+            };
 
-        //Assert  
-        Assert.IsType<ActionResult<User>>(data);
-        Assert.NotNull(data);
-        Assert.True(data.Value.Email.Equals("lucia@hotmail.com"));
+            //Act  
+            var data = controller.Create(user);
+
+            //Assert  
+            Assert.IsType<ActionResult<User>>(data);
+            Assert.NotNull(data);
+            Assert.True(data.Value.Email.Equals("lucia@hotmail.com"));
+        }
     }
+    
+    
 }
