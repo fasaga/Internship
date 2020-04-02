9fc460439f1365266a64191e0ff18df4ae38de01
Rogelio Lopez <rogelio.lopez@agilethought.com>
2020-04-02 17:41:53 -0600

    ?

--- Internship.API/Repositories/Interfaces/IInternRepository.cs
+++ Internship.API/Repositories/Interfaces/IInternRepository.cs
@@ -12,7 +12,5 @@ namespace Internship.API.Repositories.Interfaces
         Intern Get(string id);
 
         List<Intern> GetByMentorId(string mentorId);
-
-        Intern GetInternById(string id);
     }
 }
