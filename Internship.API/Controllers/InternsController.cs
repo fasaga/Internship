c4f73268f3a0856a97941abfa641ad645fe61dd4
pedro.ruiz <pedro.ruiz@agilethought.com>
2020-04-24 15:34:11 -0500

    US 45: Modify create/update endpoints to apply special characters validation

--- Internship.API/Controllers/InternsController.cs
+++ Internship.API/Controllers/InternsController.cs
@@ -62,6 +62,21 @@ namespace Internship.API.Controllers
         {
             try
             {
+                SpecialCharacters specialCharacters = new SpecialCharacters();
+                var ProjectCheck = specialCharacters.CheckSpecialCharacters(intern.Project, "CharsSpecial");
+                var TeamCheck = specialCharacters.CheckSpecialCharacters(intern.Team, "CharsSpecial");
+                var LeadCheck = specialCharacters.CheckSpecialCharacters(intern.Lead, "CharsSpecial");
+                var resourceManagerCheck = specialCharacters.CheckSpecialCharacters(intern.ResourceManager, "CharsSpecial");
+
+                if (ProjectCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", ProjectCheck));
+                else if (TeamCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", TeamCheck));
+                else if (LeadCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", LeadCheck));
+                else if (resourceManagerCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", resourceManagerCheck));
+
                 //validations section
                 //verify that the Userid is not the same as the Mentorid
                 if (intern.MentorId == intern.UserId)
@@ -158,6 +173,21 @@ namespace Internship.API.Controllers
         {
             try
             {
+                SpecialCharacters specialCharacters = new SpecialCharacters();
+                var ProjectCheck = specialCharacters.CheckSpecialCharacters(internIn.Project, "CharsSpecial");
+                var TeamCheck = specialCharacters.CheckSpecialCharacters(internIn.Team, "CharsSpecial");
+                var LeadCheck = specialCharacters.CheckSpecialCharacters(internIn.Lead, "CharsSpecial");
+                var resourceManagerCheck = specialCharacters.CheckSpecialCharacters(internIn.ResourceManager, "CharsSpecial");
+
+                if (ProjectCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", ProjectCheck));
+                else if (TeamCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", TeamCheck));
+                else if (LeadCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", LeadCheck));
+                else if (resourceManagerCheck != "true")
+                    return BadRequest(new ApiError(400, "Invalid characters", resourceManagerCheck));
+
                 //validations section
                 //verify that the Userid is not the same as the Mentorid
                 if (internIn.MentorId == internIn.UserId)
